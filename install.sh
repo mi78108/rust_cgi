550cfe462e383d0fa53628e2478fff256d6da64e
mi78108 <mi78108@live.com>
2025-04-06 22:58:06 +0800

    重写

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ mkdir -pv ~/.config/systemd/user/
 cp -v ./rust_cgi.service ~/.config/systemd/user/
 sed -i "s/\${USERNAME}/$USER/g" ~/.config/systemd/user/rust_cgi.service
 cp -v ./target/release/rust_cgi ~/Web
-cp -rv ./src/cgi ~/Web/
+rsync -auv --delete ./src/cgi ~/Web/
 
 systemctl --user daemon-reload
