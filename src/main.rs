6bb6e04a82a1c138dc1d1a23991bab4d3fff4cb8
mi78108 <mi78108@live.com>
2025-04-15 01:54:38 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::ops::{Add, AddAssign};
 use std::ptr::null;
 use std::{io, vec};
 use std::io::prelude::*;
-use std::io::{BufReader, BufWriter, Error};
+use std::io::{BufReader, BufWriter, Error, ErrorKind};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::process::{Command, Stdio};
 use std::fs::{metadata, read};
@@ -86,7 +86,7 @@ fn main() {
     }
 
     pub trait Req {
-        fn read(&self, data: &mut Vec<u8>) -> Result<usize, io::Error>;
+        fn read(&self, data: &mut Vec<u8>) -> Result<Option<usize>, io::Error>;
         fn write(&self, data: &[u8]) -> Result<usize, io::Error>;
         fn close(&self) -> Result<(), std::io::Error>;
         fn env(&self) -> &HashMap<String, String>;
@@ -114,31 +114,36 @@ fn main() {
     }
 
     impl Req for Http {
-        fn read(&self, data: &mut Vec<u8>) -> Result<usize, std::io::Error> {
+        fn read(&self, data: &mut Vec<u8>) -> Result<Option<usize>, std::io::Error> {
             if let Some(content_length) = self.env().get("Content-Length") {
                 if let Ok(length) = content_length.parse::<usize>() {
                     if let Ok(req_readed_size) = self.req_readed_size.read() {
                         if length.eq(&req_readed_size) {
-                            return Ok(0);
+                            debug!("Http Content-Length: {} read end", req_readed_size);
+                            return Ok(Some(0));
                         }
                     }
                 }
             } else {
                 debug!("no Content-Length Header set; read 0");
-                return Ok(0);
+                return Ok(Some(0));
             }
             //data.resize(self.req_buffer_size, 0);
-            let rst = self.req_reader.write().unwrap().read(data);
-            if let Ok(len) = rst {
-                self.req_readed_size.write().unwrap().add_assign(len);
+            match self.req_reader.write().unwrap().read(data) {
+                Ok(len) => {
+                    self.req_readed_size.write().unwrap().add_assign(len);
+                    Ok(Some(len))
+                }
+                Err(e) => {
+                    Err(e)
+                }
             }
-            return rst;
         }
         fn write(&self, data: &[u8]) -> Result<usize, std::io::Error> {
-            BufWriter::new(self.req_stream.try_clone().unwrap()).write(data)
+            BufWriter::new(self.req_stream.try_clone()?).write(data)
         }
         fn close(&self) -> Result<(), std::io::Error> {
-            BufWriter::new(self.req_stream.try_clone().unwrap()).flush();
+            BufWriter::new(self.req_stream.try_clone().unwrap()).flush()?;
             self.req_stream.shutdown(Shutdown::Both)
         }
         fn env(&self) -> &HashMap<String, String> {
@@ -152,45 +157,43 @@ fn main() {
 
     impl Websocket {
         fn write_with_h1(&self, h1: u8, data: &[u8]) -> io::Result<usize> {
-            let mut writer = BufWriter::new(self.http.req_stream.try_clone().unwrap());
+            let mut writer = BufWriter::new(self.http.req_stream.try_clone()?);
             let mut resp: Vec<u8> = Vec::new();
             let len = data.len();
-            //h1 +fin+rsv1+rsv2+rsv3+opcode*4+
+            //B1= +fin+rsv1+rsv2+rsv3+opcode*4+
             //fin 1末尾包 0还有后续包
-            //opcoce 4bit 0附加数据 1文本数据 2二进制数据 3-7保留为控制帧 8链接关闭 9ping apong b-f同3-7
-            if h1 > 0 {
-                resp.push(h1);
-            } else {
-                resp.push(0x81);
-            }
-            //h2 128 for mask bit
-            if len < 126 {
-                resp.push(len as u8);
-            } else {
-                if len > 125 && len < (1 << 16) {
-                    resp.push(126);
-                    // 2byte
-                    resp.push((len >> 8) as u8);
-                    resp.push(len as u8);
-                } else {
-                    if len > (1 << 16) - 1 {
-                        resp.push(127);
-                        // 8byte
-                        (0..8).for_each(|v| resp.push((len >> 8 * (7 - v)) as u8))
-                    }
+            //opcode 4bit 0附加数据 1文本数据 2二进制数据 3-7保留为控制帧 8链接关闭 9ping 0xApong b-f同3-7保留
+            resp.push(h1);
+            //B2=  +mask+len*7
+             match len {
+                n if n < 126 => {
+                    resp.push(len as u8)
                 }
-            }
-            //mask
+                n if n >= 126 && n < (2 ^ 16) -1 => {
+                    resp.push(127);
+                    // 2byte
+                    resp.extend_from_slice(&[(len >> 8) as u8,len as u8]);
+                }
+                n if n > (2 ^ 16) - 1 && n < (2 ^ 64) - 1 => {
+                    resp.push(127);
+                    // 8byte
+                    (0..=7).for_each(|v| resp.push((len >> 8 * (7 - v)) as u8));
+                }
+                _ => {
+                    return Err(ErrorKind::FileTooLarge.into());
+                }
+            };
+            //mask 服务器发送不需要
             //let _mask = [13u8, 9, 78, 108];
             //data
-            return match writer.write(resp.as_slice()) {
-                Ok(_) => {
-                    let rst = writer.write(data);
-                    writer.flush();
-                    return rst;
+            resp.extend(data);
+            match writer.write(&resp) {
+                Ok(len) => {
+                    writer.flush()?;
+                    return Ok(len);
                 }
                 Err(e) => Err(e),
-            };
+            }
         }
     }
     impl From<Http> for Websocket {
@@ -212,73 +215,89 @@ fn main() {
     }
 
     impl Req for Websocket {
-        fn read(&self, data: &mut Vec<u8>) -> Result<usize, std::io::Error> {
+        fn read(&self, data: &mut Vec<u8>) -> Result<Option<usize>, std::io::Error> {
             let mut reader = BufReader::new(self.http.req_stream.try_clone().unwrap());
-            //let mut load: Vec<u8> = Vec::new();
-            let mut _mask = [0u8; 4];
-            let mut _byte = [0u8; 1];
-            //byte 1
-            if let Ok(_) = reader.read(&mut _byte) {
-                //println!(" > websocket byte one {:b}", _byte[0]);
-                if 0b10001000 == _byte[0] {
-                    // ctrl close
-                    return Ok(0);
-                }
-                if 0b10001001 == _byte[0] {
-                    // ctrl ping 0b1000-1010
-                    self.write_with_h1(0b10001010, b"pong").unwrap();
-                    debug!("Websocket wrote pong");
-                    return self.read(data);
-                }
-                //byte 2
-                if let Ok(_) = reader.read(&mut _byte) {
-                    //println!(" websocket fram byte 2 {:b}", _byte[0]);
-                    let _length = match _byte[0] & 0x7f {
-                        n if n < 126 => n as usize,
-                        n if n == 126 => {
-                            //2byte
-                            (0..2).fold(0usize, |a, v| {
-                                while let Ok(_) = reader.read(&mut _byte) {
-                                    return a + (_byte[0] as usize) << 8 * (1 - v);
-                                }
-                                return a;
-                            })
-                        }
-                        n if n == 127 => {
-                            //8byte
-                            (0..8).fold(0usize, |a, v| {
-                                if let Ok(_) = reader.read(&mut _byte) {
-                                    return a + (_byte[0] as usize) << 8 * (7 - v);
-                                }
-                                return a;
-                            })
-                        }
-                        _ => 0,
-                    };
-                    //println!("play load  len {}", _length);
-                    //mask 4byte
-                    if let Ok(_) = reader.read(&mut _mask) {
-                        //println!("get mask {:?}", _mask);
-                        //get playload
-                        data.resize(_length, 0);
-                        while let Ok(_) = reader.read(&mut data[.._length]) {
-                            //unmask
-                            for i in 0.._length {
-                                data[i] = data[i] ^ _mask[i % 4];
-                            }
-                            return Ok(_length);
-                        }
-                    }
-                }
+            let mut bytes = [0u8; 2];
+            if let Err(e) = reader.read_exact(&mut bytes) {
+                return Err(e);
             }
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                "websocket read erro",
-            ));
+            debug!(" websocket read byte 1:{:b}",bytes[0]);
+            debug!(" websocket read byte 2:{:b}",bytes[1]);
+            let count_bytes = |bytes: &[u8]| -> usize {
+                bytes.iter().enumerate().fold(0usize, |a, (i, v)| {
+                    //debug!(" websocket read byte 8 * {} - {}  - 1",bytes.len(),i);
+                    a + ((*v as usize) << (8 * (bytes.len() - 1 - i)))
+                })
+            };
+            //part2 byte 1 [+mask,+++++++load len ]
+            let length_rst: Result<usize, Error> = match bytes[1] & 0x7f {
+                n if n < 126 => Ok(n as usize),
+                //2byte
+                n if n == 126 => {
+                    let mut bytes = [0u8; 2];
+                    reader.read_exact(&mut bytes).and(Ok(count_bytes(&bytes)))
+                }
+                n if n == 127 => {
+                    //8byte
+                    let mut bytes = [0u8; 8];
+                    reader.read_exact(&mut bytes).and(Ok(count_bytes(&bytes)))
+                }
+                _ => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "invalid data"))
+            };
+            if let Err(e) = length_rst {
+                return Err(e);
+            }
+            let length: usize = length_rst.unwrap();
+            //println!("play load  len {}", length);
+            //part3 mask 4byte
+            let mut mask = [0u8; 4];
+            if let Err(e) = reader.read_exact(&mut mask) {
+                return Err(e);
+            }
+            //println!("get mask {:?}", _mask);
+            //get play load
+            debug!("Websocket read package length {}",length);
+            data.resize(length, 0);
+            if let Err(e) = reader.read_exact(data) {
+                debug!("websocket read package length fail read bytes:{} {:?}",length,e);
+                return Err(e);
+            }
+            //unmask
+            for i in 0..data.len() {
+                data[i] = data[i] ^ mask[i % 4];
+            }
+            // frame read done
+            //byte 1 [+fin,+rsv1,+rsv2,+rsv3,++++opcode]
+            return match bytes[0] {
+                // h  if h == 0b10000000 => {
+                //     // con frame
+                //     Some(Ok(length))
+                // },
+                h  if h == 0b10000010 => {
+                    // bin frame
+                    Ok(Some(length))
+                }
+                h if h == 0b10000001 => {
+                    // text frame
+                    Ok(Some(length))
+                }
+                h if h == 0b10001000 => {
+                    //0x88 0x80 4byte_masking
+                    //ctrl close 0x8 0b10001000
+                    Err(std::io::Error::new(std::io::ErrorKind::ConnectionAborted, "connection closed"))
+                }
+                h if h == 0b10001001 => {
+                    // ctrl ping 0x9 0b10001001
+                    // ctrl pong 0xA 0b10001010
+                    Ok(None)
+                }
+                _ => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "invalid data; 不支持的扩展协议")),
+            };
         }
 
         fn write(&self, data: &[u8]) -> Result<usize, Error> {
-            self.write_with_h1(0, data)
+            // 文本 末包
+            self.write_with_h1(0x81, data)
         }
 
         fn close(&self) -> Result<(), Error> {
@@ -361,14 +380,14 @@ fn main() {
             }
         }
         // parse restful argv
-        let mut restful_argv:Vec<String> = [http.headers.get("req_path").unwrap().to_string()].to_vec();
+        let mut restful_argv: Vec<String> = [http.headers.get("req_path").unwrap().to_string()].to_vec();
         parse_req_path(&mut restful_argv);
         debug!("parseed req path {:?}",restful_argv);
         http.headers.insert(String::from("req_script_path"), restful_argv[0].to_string());
         if restful_argv.len() > 1 {
             restful_argv.remove(0);
             restful_argv.reverse();
-            restful_argv.iter().enumerate().for_each(|(i,v)| {
+            restful_argv.iter().enumerate().for_each(|(i, v)| {
                 http.headers.insert(format!("req_argv_{}", i + 1), String::from(v));
                 http.headers.insert(format!("req_param_argv_{}", i + 1), String::from(v));
             });
@@ -413,7 +432,7 @@ fn main() {
             let argv = script_file_path.file_name().unwrap().to_str().unwrap();
             parse_path.push(argv.to_string());
             script_file_path.pop();
-           //
+            //
             if script_file_path.exists() {
                 if script_file_path.is_file() {
                     //文件存在 并且是文件  ok return
@@ -434,10 +453,10 @@ fn main() {
         debug!("script_file_path while end= {:?}", script_file_path);
     }
     fn call_script(req: Box<(dyn Req + Send + Sync)>) {
-        let BUFFER_SIZE = req.env().get("Req_Buffer_Size").unwrap().parse::<usize>().unwrap_or_else(|e| 256);
+        let BUFFER_SIZE = req.env().get("Req_Buffer_Size").unwrap().parse::<usize>().unwrap_or_else(|e| 1024 * 128);
         if let Some(req_path) = req.env().get("req_script_path") {
             info!("Req [{}]", req_path);
-            let mut script = Command::new(format!(".{}", req_path.replacen(WDIR.read().unwrap().as_str(),"",1)));
+            let mut script = Command::new(format!(".{}", req_path.replacen(WDIR.read().unwrap().as_str(), "", 1)));
             script.current_dir(WDIR.read().unwrap().as_str());
             //let mut env = req.env().clone();
             script.env_clear().envs(req.env()).stdin(Stdio::piped()).stdout(Stdio::piped());
@@ -459,33 +478,31 @@ fn main() {
                             let mut buffer = Vec::new();
                             buffer.resize(BUFFER_SIZE, 0);
                             // 按缓存读取内容，避免内存溢出
-                            while let Ok(len) = req_read.read(&mut buffer) {
-                                debug!("tcpStream read len [{}] [{:?}]", len, String::from_utf8_lossy(&buffer[..len]));
-                                //debug!("tcpStream read len [{}]",len);
-                                if len > 0 {
-                                    if let Err(e) = stdin.write(&buffer[..len]) {
-                                        error!("script stdin write thread {:?} break", e);
+                            while let Ok(len_opt) = req_read.read(&mut buffer) {
+                                if let Some(len) = len_opt {
+                                    debug!("tcpStream read len [{}] [{:?}]", len, String::from_utf8_lossy(&buffer[..len]));
+                                    //debug!("tcpStream read len [{}]",len);
+                                    if len > 0 {
+                                        if let Err(e) = stdin.write(&buffer[..len]) {
+                                            error!("script stdin write thread {:?} break", e);
+                                            break;
+                                        }
+                                        debug!("script stdin write [{}]",len);
+                                        if let Err(e) = stdin.flush() {
+                                            error!("script stdin write thread flush erro {:?}; break", e);
+                                            break;
+                                        }
+                                        // fix 会引起 read 读取不到数据
+                                        //buffer.clear();
+                                    } else {
+                                        debug!("script stdin thread tcpStream read data len 0; break");
                                         break;
                                     }
-                                    debug!("script stdin write [{}]",len);
-                                    if let Err(e) = stdin.flush() {
-                                        error!("script stdin write thread flush erro {:?}; break", e);
-                                        //drop(stdin);
-                                        break;
-                                    }
-                                    // test
-                                    // if req_body_method.eq("WEBSOCKET") {
-                                    //     stdin.write(&[0x0]).unwrap();
-                                    //     stdin.flush().unwrap();
-                                    //     debug!(">>>>>>>>>>>>>>>>>>>> : write [] done");
-                                    // }
-                                    buffer.clear();
                                 } else {
-                                    debug!("script stdin thread tcpStream read data len 0; break");
-
-                                    break;
+                                    // 忽略None， 直接再次读取
                                 }
                             }
+                            // 读错误， 忽略并结束
                         }
                     });
                     //
