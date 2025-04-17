bcf7816d0ca26e287bff5161b13a99a13939f8bf
mi78108 <mi78108@live.com>
2025-04-18 05:15:19 +0800

    fix

--- src/cgi/index/index.rb
+++ src/cgi/index/index.rb
@@ -4,112 +4,25 @@
 
 require 'redis'
 require 'json'
-require 'connection_pool'
 require_relative '../_base'
 
 
-key = $$.to_s
 uid = $$.to_s
 #全局广播
 #聊天室
 Q.on :WEBSOCKET do |r|
-    if Q.param('argv_1') == 'room'
-        if Q.param('argv_2')
-            key = Q.param('argv_2')
-        end
-    end
-
-  redis ||= ConnectionPool::Wrapper.new do
-      Redis.new
-  end
-
-  thread = Thread.new do
-      call_func = lambda do |c, val|
-          if Q.param('argv_1') == 'room'
-              return if c == 'client_broadcast'
-            if c == "client_#{key}"
-              info = JSON.parse(val)
-              return if info['from'] == uid
-            end
-          end
-
-        if c == 'client_opt'
-            Q.send ({'opt' => 'info', 'value' => { 'value' => val }}).to_json
-        end
-        if c == 'client_broadcast'
-            info = JSON.parse(val)
-            return if uid == info['from']
-            Q.send ({'opt' => 'text', 'value' => info['value'], 'from' => info['from'], 'to' => info['to'] }).to_json
-        end
-        if c == "client_#{key}"
-            info = JSON.parse(val)
-            Q.send ({'opt' => 'text', 'value' => info['value'], 'from' => info['from'], 'to' => info['to'] }).to_json
-        end
-      end
-      redis.subscribe('client_opt', 'client_broadcast', "client_#{key}") do |on|
-        on.message do |c, m|
-            STDERR.puts ">>>>>>>>>>>>>>>>>>>>>>> redis sub recv #{c} #{m}"
-            val = m.force_encoding('utf-8')
-            call_func.call(c, val)
-        end
-      end
-  end
-
-  Q.on_close do
-      Thread::kill thread
-      #redis = Redis.new
-      redis.publish 'client_opt', "#{uid} 已离线"
-      count = redis.hget('clients', 'count').to_i
-      redis.hset('clients', 'count', count - 1)
-      redis.hdel 'clients', "#{uid}"
-      redis.close
-  end
-
-
-  redis.publish 'client_opt', "#{uid} 已上线"
-  redis.hset 'clients', "#{uid}", 'online'
-  count = redis.hget('clients', 'count').to_i + 1
-  redis.hset('clients', "count", count)
-
-  Q.send({'opt' => 'text', 'value'=> "#{uid} 已上线"}.to_json)
-  Q.send ({'opt' => 'set', 'value' => {'uid' => uid, 'room' => key} }).to_json
-  if Q.param('argv_1') == 'room'
-    #Q.send ({'opt' => 'set', 'value' => {'sendTo' => "#{key}"} }).to_json
-  else
-    Q.send ({'opt' => 'info', 'value' => redis.hgetall('clients') }).to_json
-  end
-
   Q.recv do |data|
-      info = JSON.parse data
-      case info['opt']
-          when 'input'
-              if Q.param('argv_1') == 'room'
-                redis.publish "client_#{key}", ({'from' => uid, 'to' => key,'value' => info['value']}).to_json
-              else
-                redis.publish "client_broadcast",    ({'from' => uid ,'to' => 'broadcast','value' => info['value']}).to_json
-              end
-
-              info['opt'] = 'info'
-              info['value'] = {'result': '发送成功'}
-              Q.send info.to_json
-          when 'send'
-              STDERR.puts info.to_json
-              redis.publish "client_#{info['to']}" , ({'from' => uid,'to' => info['to'],'value' => info['value']}).to_json
-              Q.send ({'opt' => 'cmd', 'value' => {'result': '发送成功'} }).to_json
-          when 'cmd'
-              case info['value']
-                  when '@refresh'
-                      Q.send ({'opt' => 'info', 'value' => redis.hgetall('clients') }).to_json
-                  else
-                      Q.send ({'opt' => 'cmd', 'value' => 'unSupport'}).to_json
-              end
-          else
-              Q.send info.to_json
-      end
   end
 end
 
+Q.on :POST do |r|
+    data = Hash.new
+    data['type'] = Q.param('argv_1')
+    data['room'] = Q.param('argv_2')
+    resp_ok :json, data.to_json
+end
+
 Q.on :GET do |r|
-  resp_ok :html, File::read('./index/page.html')
+    resp_ok :html, File::read('./index/page.html')
 end
 
--- src/cgi/room/room.rb
+++ src/cgi/room/room.rb
@@ -14,17 +14,26 @@ room = Q.param_or('argv_1', 'default')
 Q.on :WEBSOCKET do |r|
   thread = Thread.new do
       redis = Redis.new
-      redis.subscribe('client_opt', 'client_broadcast', "client_#{room}") do |on|
+      redis.subscribe('client_opt', "client_#{room}", "client_#{uid}") do |on|
         on.message do |c, val|
             STDERR.puts ">>>>>>>>>>>>>>>>>>>>>>> redis sub recv #{uid} #{c} #{val}"
             begin
                 val = JSON.parse val.force_encoding('utf-8')
                 if val['from'] != uid
                     if c == "client_#{room}"
+                        # 同一房间 下发信息
                         STDERR.puts ">>>>>>>>>>>>>>>>>>>>>>> redis sub send"
+                        val['type'] = 'room'
+                        Q.send val.to_json
+                    end
+                    if c == "client_#{uid}"
+                        # 指定发送 下发信息
+                        val['type'] = 'private'
                         Q.send val.to_json
                     end
                     if c == 'client_opt'
+                        # 操作信息 全部下发
+                        val['type'] = 'broadcast'
                         Q.send val.to_json
                     end
                 end
@@ -44,32 +53,36 @@ Q.on :WEBSOCKET do |r|
 
   Q.send ({'opt' => 'text', 'value'=> "#{uid} 已上线"}).to_json
   Q.send ({'opt' => 'set', 'value' => {'uid' => uid, 'room' => room}}).to_json
-  Q.send ({'opt' => 'info', 'value' => redis.hgetall("client_#{room}")}).to_json
+  Q.send ({'opt' => 'info', 'event' => 'click' ,'value' => redis.hgetall("client_#{room}")}).to_json
 
   Q.recv do |data|
       info = JSON.parse data
       case info['opt']
           when 'input'
-              redis.publish "client_#{room}", ({'opt' => 'text', 'from' => uid ,'to' => room,'value' => info['value']}).to_json
-              info['opt'] = 'info'
-              info['value'] = {'Status': '发送成功'}
-          when 'send'
-              STDERR.puts info.to_json
-              redis.publish "client_#{info['to']}" , ({'from' => uid,'to' => info['to'],'value' => info['value']}).to_json
+              redis.publish "client_#{room}", ({'opt' => 'text', 'from' => uid ,'to' => room, 'value' => info['value']}).to_json
               info['opt'] = 'info'
               info['value'] = {'Status': '发送成功'}
           when 'cmd'
               case info['value']
                   when '@refresh'
                       info['opt'] = 'info'
+                      info['cmd'] = '@refresh'
                       info['value'] = redis.hgetall("client_#{room}")
+                  when '@send'
+                      redis.publish "client_#{info['to']}" , ({'opt' => 'text', 'cmd'=>'@send', 'from' => uid, 'room' => room, 'to' => info['to'], 'value' => info['text']}).to_json
+                      info['opt'] = 'info'
+                      info['rst'] = {'Status': '发送成功'}
+                  when '@call'
+                      redis.publish "client_#{info['to']}" , ({'opt' => 'info', 'cmd' => '@call', 'from' => uid, 'room' => info['room'], 'to' => info['to'], 'value' => {'Notice' => 'Invites',"value" => info['text']}}).to_json
+                      info['opt'] = 'info'
+                      info['rst'] = {'Status': '发送成功'}
                   else
                       info['opt'] = 'info'
-                      info['value'] = 'unSupport'
+                      info['rst'] = {text: 'unSupport'}.to_json
               end
           else
               info['opt'] = 'info'
-              info['value'] += ' 不支持'
+              info['rst'] = ' 不支持'
       end
       Q.send info.to_json
   end
