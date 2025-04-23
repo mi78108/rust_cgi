826226c9e9f53ce24f01221fef7a1e038154811a
mi78108 <mi78108@live.com>
2025-04-23 18:34:41 +0800

    同步

--- src/cgi/index/index.rb
+++ src/cgi/index/index.rb
@@ -2,10 +2,9 @@
 
 # -*- coding: utf-8 -*-
 
-require 'redis'
-require 'json'
-require_relative '../_base'
-
+require "redis"
+require "json"
+require_relative "../_base"
 
 uid = $$.to_s
 #全局广播
@@ -16,13 +15,12 @@ Q.on :WEBSOCKET do |r|
 end
 
 Q.on :POST do |r|
-    data = Hash.new
-    data['type'] = Q.param('argv_1')
-    data['room'] = Q.param('argv_2')
-    resp_ok :json, data.to_json
+  data = Hash.new
+  data["type"] = Q.param("argv_1")
+  data["room"] = Q.param("argv_2")
+  resp_ok :json, data.to_json
 end
 
 Q.on :GET do |r|
-    resp_ok :html, File::read('./index/page.html')
+  resp_ok :html, File::read("./index/page.html")
 end
-
