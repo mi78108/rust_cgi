4fd03adeae0c002cebf7eb3ffdb20168c7428b87
mi78108 <mi78108@live.com>
2025-05-20 18:40:13 +0800

    add udp suppost

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use clap::{App, Arg};
-use std::net::{TcpListener, TcpStream};
-use std::process::{Command, Stdio};
-use std::sync::RwLock;
-use std::{io, process, vec};
-use std::fmt::format;
+use std::net::{TcpListener,  UdpSocket};
+use std::sync::OnceLock;
+use std::thread::spawn;
 
 #[macro_use]
 extern crate log;
-#[macro_use]
-extern crate lazy_static;
 
-mod http_func;
 mod tcp_class;
-lazy_static! {
-    static ref WDIR: RwLock<String> = RwLock::new(String::from("/tmp"));
-}
+mod udp_class;
+mod utils;
+
+static WDIR: OnceLock<String> = OnceLock::new();
 // todo
 fn main() {
     env_logger::init();
@@ -23,39 +19,38 @@ fn main() {
         .author("mi78108@live.com>")
         .arg(
             Arg::with_name("workdir")
-                .short("f")
-                .long("cgi")
-                .help("www cgi work dir")
-                .takes_value(true).default_value("./"),
+            .short("f")
+            .long("cgi")
+            .help("www cgi work dir")
+            .takes_value(true).default_value("./"),
         )
         .arg(
             Arg::with_name("addr")
-                .short("l")
-                .long("addr")
-                .help("bind address")
-                .takes_value(true),
+            .short("l")
+            .long("addr")
+            .help("bind address")
+            .takes_value(true),
         )
         .arg(
             Arg::with_name("host")
-                .short("h")
-                .long("host")
-                .help("bind host address")
-                .takes_value(true).conflicts_with("addr").requires("port")
+            .short("h")
+            .long("host")
+            .help("bind host address")
+            .takes_value(true).conflicts_with("addr").requires("port")
         )
         .arg(
             Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .help("bind port address")
-                .takes_value(true).conflicts_with("addr").requires("host")
+            .short("p")
+            .long("port")
+            .help("bind port address")
+            .takes_value(true).conflicts_with("addr").requires("host")
         )
         .get_matches();
 
     if let Some(wd) = matches.value_of("workdir") {
-        if let Ok(mut wwd) = WDIR.write() {
-            wwd.clear();
-            wwd.push_str(wd);
-        };
+        WDIR.get_or_init(||{
+            wd.to_string()
+        });
         info!("set workdir [{}]", wd);
     }
 
@@ -65,15 +60,21 @@ fn main() {
     };
     let addr = addr.as_str();
 
-    let listener = TcpListener::bind(addr).expect(format!("bind {} erro", addr).as_str());
-    info!("Listen on [{}] Work in [{}]", addr, WDIR.read().unwrap());
-    for stream in listener.incoming() {
+    let udp_listener = UdpSocket::bind(addr).expect(format!("udp bind {} erro",addr).as_str());
+    spawn(move ||{
+        udp_listener.set_broadcast(true).unwrap();
+        udp_class::udp_base::handle(udp_listener);
+    });
+
+    let tcp_listener = TcpListener::bind(addr).expect(format!("bind {} erro", addr).as_str());
+    info!("Listen on [{}] Work in [{}]", addr, WDIR.get().unwrap());
+    for stream in tcp_listener.incoming() {
         match stream {
             Ok(_stream) => {
                 std::thread::spawn(move || {
-                    println!("call start new Req thread started");
-                    handle(_stream);
-                    println!("call end handle Req thread ended\r\n\r\n");
+                    debug!("tcp call start new Req thread started");
+                    tcp_class::tcp_base::handle(_stream);
+                    debug!("tcp call end handle Req thread ended\r\n\r\n");
                 });
             }
             Err(e) => {
@@ -82,20 +83,5 @@ fn main() {
         };
     }
 
-    fn handle(stream: TcpStream) {
-        let mut buffer = [0u8; 16];
-        if let Err(e) = stream.peek(&mut buffer) {
-            error!("Tcp handle read erro {:?}", e)
-        }
-        if String::from_utf8_lossy(&buffer[..])
-            .to_uppercase()
-            .contains("HTTP")
-        {
-            debug!("Tcp Req Handled With HTTP");
-            tcp_class::call_script(http_func::parse_req(stream));
-        } else {
-            debug!("Tcp Req Handled With Tcp default");
-            tcp_class::call_script(tcp_class::parse_req(stream));
-        }
-    }
+    
 }
