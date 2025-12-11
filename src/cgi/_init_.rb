d49c3a1482d97c93caece519d6bf88bd2bcaee98
ZhangLinag <mi78108@live.com>
2025-12-11 18:20:27 +0800

    add redirect

--- src/cgi/_init_.rb
+++ src/cgi/_init_.rb
@@ -3,9 +3,9 @@ require 'json'
 require "pathname"
 
 class Req
-  def initialize()
-    @path = URI::decode_uri_component(ENV['req_path'])
-    @method = ENV['req_body_method'] == 'HTTP' ? ENV['req_method'] : ENV['req_body_method']
+  def initialize(path = Q::REQ_PATH)
+    @path = path
+    @method = Q::REQ_METHOD
     @argvs = ENV.keys.filter {|k| k =~ /req_argv_\d+/ }.sort.map{ |k| URI::decode_uri_component(ENV[k]) }
     @params = ENV['req_params'] ? URI::decode_uri_component(ENV['req_params']).split('&').map {|v| v.split('=')[1]} : Array.new 
   end
@@ -78,6 +78,18 @@ class Rsp
     @body = body
     return self
   end
+  def render(&block)
+    body(Q.render @body, &block)
+    return self
+  end
+  def json
+    type 'application/json; charset=utf-8'
+    return self
+  end
+  def html
+    type 'text/html; charset=utf-8'
+    return self
+  end
   def ok_json body
     type 'application/json; charset=utf-8'
     ok body.to_json
@@ -100,51 +112,65 @@ module Q
   BUFFER_SIZE = 10 * 1024 * 1024
   REQ_PATH = URI::decode_uri_component(ENV['req_path'])
   REQ_BODY_METHOD = ENV['req_body_method']
+  REQ_ARGV_PARAMS = URI::decode_uri_component(ENV['req_argv_params'])
   REQ_METHOD = REQ_BODY_METHOD == 'HTTP' ? ENV['req_method'] : ENV['req_body_method']
+  SCRIPT_DIR = ENV['req_script_dir']
+  SCRIPT_NAME = ENV['req_script_name'] 
+  SCRIPT_PATH = ENV['req_script_path']
+  SCRIPT_BASENAME = ENV['req_script_basename'] 
   @@UNMAP = true
   @RESP = Rsp.new
 
   def self.handle_response
     if @@UNMAP
-      resp_501 'unHandle'
+      Q.fail_501 'unHandle'
     else
       @RESP.finally unless @RESP.header['send'] && REQ_BODY_METHOD == 'HTTP'
     end
   end
-  
-#  def Q.const_missing( name )
-#    STDERR.puts "const #{name} NOT EXIST; Find in ENV"
-#    URI::decode_uri_component(ENV[name.to_s] ? ENV[name.to_s] : ENV[name.to_s.downcase])
-#  end
+
+  #  def Q.const_missing( name )
+  #    STDERR.puts "const #{name} NOT EXIST; Find in ENV"
+  #    URI::decode_uri_component(ENV[name.to_s] ? ENV[name.to_s] : ENV[name.to_s.downcase])
+  #  end
 
 
   def Q.call_block
-      @@UNMAP = false
-      begin
-        yield(Req.new, @RESP) if block_given?
-      rescue StandardError => e
-        Q.resp_501 e.to_s
-      end
+    begin
+      yield(Req.new, @RESP) if block_given?
+  # rescue StandardError => e
+    rescue Exception => e
+      Q.log e
+      Q.fail_500 e.to_s
+    end
   end
 
-  def Q.map(method=nil, *paths, &block)
-    Q.log "Ready Map #{method} with #{paths}"
-    if method.nil? and paths.empty?
+  def Q.map(method=nil, *path_matches, &block)
+    map_dir = "./#{Q::SCRIPT_NAME.sub("/#{Q::SCRIPT_BASENAME}","")}"
+    map_path = Q::REQ_PATH.sub(Q::REQ_PATH.sub("/#{Q::REQ_ARGV_PARAMS}",""), "")
+    map_path = '/' if map_path.empty?
+    Q.log "Ready Map #{map_path} on #{method} with #{path_matches}"
+    Dir.chdir(map_dir) if Dir.exist? map_dir
+    if method.nil? and path_matches.empty?
       Q.log "Mapped on default"
       Q.call_block(&block)
       return Q
     end
     if method.to_s == Q::REQ_METHOD
-      for path in paths do                
-        if (path == Q::REQ_PATH if path.instance_of? String) || (path =~ Q::REQ_PATH if path.instance_of? Regexp) || (path.call(Q::REQ_PATH) if path.instance_of? Proc)
-          (ENV['REQ_URI_MATCH'] = path.match(Q::REQ_PATH).to_a.to_json) if path.instance_of? Regexp
-          Q.log "Mapped #{method} on #{path}"
+      for match in path_matches do                
+        match_result = case match
+                       when String then match == map_path
+                       when Regexp then (match_data = match.match(map_path)) && (ENV['REQ_URI_MATCH'] = match_data.to_a.to_json; true)
+                       when Proc then match.call map_path
+                       else false
+                       end
+        if match_result
+          @@UNMAP = false
+          Q.log "Mapped #{method} on #{match}"
           Q.call_block(&block)
           return Q
         end
       end
-      Q.log "Mapped #{method} on default"
-      Q.call_block(&block)
       return Q
     end
     return Q
@@ -162,12 +188,13 @@ module Q
     return count
   end
   def Q.resp(code, status, header, content)
+    return if @RESP.header['send']
     Q.write ({
       "Connection" => 'close',
       "Content-Type" => 'text/html; charset=utf-8',
       "Content-Length" => content.nil? ? 0 : content.bytesize
     }.merge(header).reduce("HTTP/1.0 #{code} #{status}\r\n") {|a, (k,v)|
-        a + "#{k}: #{v}\r\n"
+      a + "#{k}: #{v}\r\n"
     } + "\r\n"), false
 
     Q.write content, true
@@ -185,17 +212,44 @@ module Q
   def Q.ok_html(body, header = {})
     Q.ok 'text/html; charset=utf-8', body, header
   end
-  def Q.resp_404(body, header={})
+  def Q.fail_404(body, header={})
     header['Content-Type'] = 'text/html'
     Q.resp 404, 'Not Found', header, body
   end
-  def Q.resp_500(body, header={})
+  def Q.fail_500(body, header={})
     header['Content-Type'] = 'text/html'
     Q.resp 500, 'Internal Server Error', header, body
   end
-  def Q.resp_501(body, header={})
+  def Q.fail_501(body, header={})
     header['Content-Type'] = 'text/html'
     Q.resp 501, 'Not Implemented', header, body
+  end
+  def Q.redirect_301(body, header={})
+    header['Content-Type'] = 'text/html'
+    header['Location'] = body
+    Q.resp 301, 'Moved Permanently', header, body
+  end
+  def Q.redirect_302(body, header={})
+    header['Content-Type'] = 'text/html'
+    header['Location'] = body
+    Q.resp 302, 'Found', header, body
+  end
+
+  def Q.render(body, v={})
+    def v.method_missing(name, *args)
+      if name.to_s.end_with?("=")
+        key = name.to_s.chomp("=").to_sym
+        self[key] = args.first
+      else
+        self[name.to_sym] || self[name.to_s] # 兼容符号/字符串键
+      end
+    end
+
+    def v.respond_to_missing?(name, include_private = false)
+      true
+    end
+    yield(v) if block_given?
+    return eval(%Q{"#{body}"})
   end
 
   def Q.recv
