e0aa3d952cadf97415b2e31aa9832d4c7ea46529
mi78108 <mi78108@live.com>
2026-01-07 22:22:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
-mkdir -pv ~/Web
-mkdir -pv ~/.config/systemd/user/
-cp -v ./rust_cgi.service ~/.config/systemd/user/
+mkdir -pv ~/.cgi
+mkdir -pv ~/.local/bin
+mkdir -pv ~/.config/systemd/user
+cp -v ./rust_cgi.service ~/.config/systemd/user
 sed -i "s/\${USERNAME}/$USER/g" ~/.config/systemd/user/rust_cgi.service
 cargo build --release
 systemctl --user stop rust_cgi.service
-cp -v ./target/release/rust_cgi ~/Web
-rsync -auv --delete ./src/cgi ~/Web/
+cp -v ./target/release/spider ~/.local/bin/rust_cgi
+rsync -auv --delete ./cgi ~/.cgi
 
 systemctl --user daemon-reload
