800bd07f37b23e1abd91d2fdcfd15636ea183665
mi78108 <mi78108@live.com>
2026-03-18 16:37:52 +0800

    sync fix render matcher

--- cgi/_init_.rb
+++ cgi/_init_.rb
@@ -140,7 +140,8 @@ class Rsp
     Q.log "instance_variables_self:", self.binding.instance_variables
     Q.log "instance_variables_resp_final:", instance_variables
 
-    @body.gsub!(/@{(?<code>.*?)}/m) do |match|
+    #@body.gsub!(/@{(?<code>.*?)}/m) do |match|
+    @body.gsub!(/@{(?<code>(?>[^{}]|\{[^}]*\})*)}/) do |match|
       code = $~[:code]
       Q.log "template matched #{match} code: [#{code}]"
       begin
